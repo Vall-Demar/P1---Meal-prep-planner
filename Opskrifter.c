50ee4138a15e91f5ed518be9bf26009a1b8fe748
SukhanoV1-RUS <g16730395@gmail.com>
2025-11-05 18:10:19 +0100

    Update Opskrifter.c

--- Opskrifter.c
+++ Opskrifter.c
@@ -2,8 +2,10 @@
 #include "opskrift.h"
 
 void initOpskrifter(Opskrift opskrifter[], int antal) {
-    if (antal < 9) return;
-    ...
+    if (antal < 9) return;  // paramerter fra at den kan vælge mellem  9 retter:
+    // Skriv kode fra her:
+
+    // skriv kode, skal have navn på opskrift, tid ,indgredienser , fremgangsmåde, pris
 }
 
 

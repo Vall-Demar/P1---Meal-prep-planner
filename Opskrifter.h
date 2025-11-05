26530a5c01af67ea8757cfcf5b7af8803075116d
SukhanoV1-RUS <g16730395@gmail.com>
2025-11-05 18:14:45 +0100

    Update Opskrifter.h

--- Opskrifter.h
+++ Opskrifter.h
@@ -1,1 +1,14 @@
+// Sørger for, at filen kun inkluderes én gang
 
+
+// Definition af en struktur til opskrifter
+
+
+// Funktion til at initialisere et array med standardopskrifter
+
+
+
+// Funktion til at vise opskrifter, der passer indenfor budget og tid
+
+
+// OPSKRIFT_H

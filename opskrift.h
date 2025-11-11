1f75710703cf35705218623241271c49ab9d9de3
SukhanoV1-RUS <g16730395@gmail.com>
2025-11-11 20:47:51 +0100

    Update opskrift.h

--- opskrift.h
+++ opskrift.h
@@ -7,9 +7,11 @@ typedef struct {
     char ingredienser[200];
     char fremgangsmåde[300];
     int pris;           // kr
+    int portioner;      // portioner man vælger
 } Opskrift;
 
 void initOpskrifter(Opskrift opskrifter[], int antal);
-void visOpskrifter(Opskrift opskrifter[], int antal, int budget, int tid);
+int antalMatchendeIngredienser(const char *harIngredienser, const char *opskriftIngredienser);
+void visOpskrifter(Opskrift opskrifter[], int antal, int budget, int tid, int ønskedePortioner, const char *harIngredienser);
 
 #endif

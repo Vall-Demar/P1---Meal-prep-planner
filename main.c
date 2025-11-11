2f6cb4b1c64f7728d16cc4af8095f1da48a2847e
SukhanoV1-RUS <g16730395@gmail.com>
2025-11-11 20:47:31 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "opskrift.h"
 
 int main() {
-    int budget, tid;
-    Opskrift opskrifter[9];
+    int budget, tid, ønskedePortioner;
+    char harIngredienser[200];
+    Opskrift opskrifter[50];
 
-    initOpskrifter(opskrifter, 9);
+    initOpskrifter(opskrifter, 50);
 
     printf("Indtast dit budget (kr): ");
     scanf("%d", &budget);
     printf("Indtast hvor lang tid du har til at lave mad (minutter): ");
     scanf("%d", &tid);
+    printf("Hvor mange portioner vil du lave? ");
+    scanf("%d", &ønskedePortioner);
+    getchar(); // fanger newline
 
-    visOpskrifter(opskrifter, 9, budget, tid);
+    printf("Skriv hvilke ingredienser du allerede har (adskilt med komma): ");
+    fgets(harIngredienser, sizeof(harIngredienser), stdin);
+    harIngredienser[strcspn(harIngredienser, "\n")] = '\0'; // fjern newline
+
+    visOpskrifter(opskrifter, 25, budget, tid, ønskedePortioner, harIngredienser);
 
     return 0;
 }
